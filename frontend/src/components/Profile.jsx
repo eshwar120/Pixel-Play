3839c8d9d4491e768aa48ac365523affd47af5a1
Eshwar <eshwaramahendra0120@gmail.com>
2023-08-09 16:13:41 +0530

    uploaded all the images to firebase 	deleted:    frontend/src/assets/cod.jpg 	deleted:    frontend/src/assets/fortnite.jpg 	deleted:    frontend/src/assets/gaming.jpg 	deleted:    frontend/src/assets/gow.jpg 	deleted:    frontend/src/assets/gta-6.jpg 	deleted:    frontend/src/assets/sale-1.jpg 	deleted:    frontend/src/assets/sale-2.jpg 	modified:   frontend/src/components/CarouselContainer.jsx 	modified:   frontend/src/components/Profile.jsx 	modified:   frontend/src/styles/signin.css

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -16,7 +16,7 @@ export default function Profile() {
       {
         loginStatus ?
           <div className='d-flex flex-column align-items-center justify-content-center vh-100-nav p-5 gap-lg-4 gap-sm-3 gap-2 text-light'>
-            <img src={profile} alt={userData.name} />
+            <img src={"https://firebasestorage.googleapis.com/v0/b/pixel-play-9891f.appspot.com/o/Banner%2Fboy.png?alt=media&token=58e2c90e-59be-43c5-bb0b-3e95e94945f5"} alt={userData.name} />
             <h2 className='text-capitalize'>Hello {userData.name}</h2>
             <Link to={"/orders"} className='text-decoration-none'>
               <button className='btn w-150px btn-outline-success gap-2 d-flex text-light justify-content-center align-items-center'>
